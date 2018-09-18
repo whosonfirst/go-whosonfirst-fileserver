b5b9f7929b9092d195a36b8a548ffe2fc55fa154
thisisaaronland <devnull@localhost>
2018-09-18 09:43:52 -0700

    stub out TLS stuff - does not work

--- cmd/wof-fileserver.go
+++ cmd/wof-fileserver.go
@@ -14,6 +14,7 @@ func main() {
 	var host = flag.String("host", "localhost", "Hostname to listen on")
 	var port = flag.Int("port", 8080, "Port to listen on")
 	var path = flag.String("path", "./", "Path served as document root.")
+	var tls = flag.Bool("tls", false, "...")
 
 	flag.Parse()
 
@@ -32,7 +33,18 @@ func main() {
 	address := fmt.Sprintf("%s:%d", *host, *port)
 	log.Printf("listening on %s\n", address)
 
-	err = http.ListenAndServe(address, mux)
+	if *tls {
+
+		// see also: https://github.com/Shyp/generate-tls-cert
+		
+		cert := "fixme"
+		key := "fixme"
+
+		err = http.ListenAndServeTLS(address, cert, key, mux)
+		
+	} else {
+		err = http.ListenAndServe(address, mux)
+	}
 
 	if err != nil {
 		log.Fatal(err)
