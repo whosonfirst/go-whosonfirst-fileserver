30e25b6f27f5676c714e8fe739ff5a56de77c6eb
thisisaaronland <devnull@localhost>
2019-08-02 18:05:36 -0700

    start figuring out TLS stuff...

--- cmd/www-fileserver/main.go
+++ cmd/www-fileserver/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"github.com/NYTimes/gziphandler"
 	"github.com/rs/cors"
+	"golang.org/x/crypto/acme/autocert"
 	"log"
 	"net/http"
 	"os"
@@ -20,8 +22,8 @@ func main() {
 	var enable_gzip = flag.Bool("gzip", false, "gzip response bodies")
 	var enable_cors = flag.Bool("cors", false, "Enable CORS header on responses")
 	var cors_origins = flag.String("cors-allowed-origins", "*", "Comma-separated list of CORS allowed origins")
-	
-	// var tls = flag.Bool("tls", false, "...")
+
+	var enable_tls = flag.Bool("tls", false, "...")
 
 	flag.Parse()
 
@@ -43,33 +45,68 @@ func main() {
 		c := cors.New(cors.Options{
 			AllowedOrigins: strings.Split(*cors_origins, ","),
 		})
-		
+
 		fs_handler = c.Handler(fs_handler)
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/", fs_handler)
 
-	address := fmt.Sprintf("%s:%d", *host, *port)
-	log.Printf("listening on %s\n", address)
+	if *enable_tls {
 
-	/*
-	if *tls {
+		// https://godoc.org/golang.org/x/crypto/acme/autocert
+		// https://letsencrypt.org/docs/certificates-for-localhost/
+		// https://github.com/FiloSottile/mkcert
+		// https://blog.filippo.io/mkcert-valid-https-certificates-for-localhost/
+		// https://github.com/golang/go/issues/20640
 
-		// see also: https://github.com/Shyp/generate-tls-cert
+		/*
 
-		cert := "fixme"
-		key := "fixme"
+		> make tools ; sudo ./bin/www-fileserver -tls -host aa.local -path ./
+		go build -mod vendor -o bin/www-fileserver cmd/www-fileserver/main.go
+		2019/08/02 18:02:42 listening on aa.local:443
+		2019/08/02 18:02:58 NAME aa.local
+		2019/08/02 18:03:03 http: TLS handshake error from 127.0.0.1:57254: 400 urn:acme:error:malformed: Error creating new authz :: Name does not end in a public suffix
 
-		err = http.ListenAndServeTLS(address, cert, key, mux)
+		*/
+		
+		*port = 443
+		
+		address := fmt.Sprintf("%s:%d", *host, *port)
+		log.Printf("listening on %s\n", address)
+		
+		m := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: autocert.HostWhitelist(*host),
+			Cache:      autocert.DirCache("./certs"),
+		}
+
+		s := &http.Server{
+			Addr:      ":https",
+			Handler: mux, 
+			TLSConfig: &tls.Config{
+				GetCertificate: m.GetCertificate,
+				MinVersion:               tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				},
+				PreferServerCipherSuites: true,
+			},
+		}
+
+		err = s.ListenAndServeTLS("", "")
 
 	} else {
+
+		address := fmt.Sprintf("%s:%d", *host, *port)
+		log.Printf("listening on %s\n", address)
+		
 		err = http.ListenAndServe(address, mux)
 	}
-	*/
-	
-	err = http.ListenAndServe(address, mux)
-	
+
+	// err = http.ListenAndServe(address, mux)
+
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.12
 require (
 	github.com/NYTimes/gziphandler v1.1.1
 	github.com/rs/cors v1.6.0
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
 )
